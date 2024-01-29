d565ea95f3a12e309e7da246402aea3fb5a8a1d3
John <johnnkonge2020@gmail.com>
2024-01-29 07:11:09 +0300

    intregrated authentication

--- affinidi-q2/index.js
+++ affinidi-q2/index.js
@@ -5,3 +5,63 @@ const session = require('express-session');
 const { Issuer, Strategy } = require('openid-client');
 const http = require("http");
 require("dotenv").config();
+
+//  2. Create an express application and set up session
+const app = express();
+app.use(session({
+    secret: 'my-secret',
+    resave: false ,
+    saveUninitialized: false
+}));
+
+// 3. Define application end-points
+app.get("/", (req, res) => {
+    res.send(" Lets build a Unified Digital Identity <br/><br/><a href='/login'> Affinidi Login</a>");
+})
+
+app.get('/login',
+    function (req, res, next) {
+        next();
+    },
+);
+
+app.get('/login/callback', (req, res, next) => {})
+
+app.get("/protected", (req, res) => {
+    res.header("Content-Type", 'application/json');
+    res.end("Protected Content for Authenticated Users");
+})
+
+// 4. Start the http server
+const httpServer = http.createServer(app)
+httpServer.listen(8080, () => {
+    console.log(`Hello World - Affinidi Login : Up and running on 8080`)
+})
+
+// 5. Integrate Authentication
+// 5a. Discover Affinidi Login - Issuer
+Issuer.discover(process.env.issuer).then(function (oidcIssuer) {
+    // 5b. Create a RP-client which can initiate an OIDC flow
+    var client = new oidcIssuer.Client({
+      client_id: process.env.client_id,
+      client_secret: process.env.client_secret,
+      redirect_uris: ["http://localhost:8080/login/callback"],
+      response_types: ['code'],
+      token_endpoint_auth_method: 'client_secret_post'
+    });
+  
+    // 5c. Provide this strategy to the passport middleware
+    passport.use(
+      'affinidi-login', new Strategy({ client, passReqToCallback: true }, (req, tokenSet, userinfo, done) => {
+        req.session.tokenSet = tokenSet;
+        req.session.userinfo = userinfo;
+        return done(null, tokenSet.claims());
+      }));
+  });
+  
+  passport.serializeUser(function (user, done) {
+    done(null, user);
+  });
+  passport.deserializeUser(function (user, done) {
+    done(null, user);
+  });
